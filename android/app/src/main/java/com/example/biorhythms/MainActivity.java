cc4311fbba9a879cfc229a71a8e4abb8981c9b30
Diego Faravelli <diego.faravelli@oncode.it>
2024-02-24 16:13:54 +0100

    initial commit

---
+++ android/app/src/main/java/com/example/biorhythms/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.biorhythms;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/lib/hooks.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export const useStoredState = (storageKey, defaultState) => {
+    const getInitialState = () => {
+        const storedState = localStorage.getItem(storageKey);
+        return storedState ?? defaultState;
+    }
+    const [state, setState] = useState(getInitialState);
+    const setAndStoreState = (state)  => {
+        setState(state);
+        localStorage.setItem(storageKey, state);
+    }
+    return [state, setAndStoreState];
+}
